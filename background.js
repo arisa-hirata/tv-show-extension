3a48f4828217f09af59f5bac0c6833c01a87cb7f
Arisa Hirata <arisa.pirata@gmail.com>
2021-06-13 13:30:32 -0700

    Message Passing

--- background.js
+++ background.js
@@ -11,4 +11,10 @@ chrome.runtime.onInstalled.addListener((details) => {
 	});
 });
 
-console.log("background script running");
+
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+	console.log(msg);
+	console.log(sender);
+	sendResponse("receive message from background");
+	chrome.tabs.sendMessage(sender.tab.id, "Got your message from background!")
+});
