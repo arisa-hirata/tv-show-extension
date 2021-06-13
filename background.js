9fd62e21cb8cd3100ac971c42be5782c8af610b3
Arisa Hirata <arisa.pirata@gmail.com>
2021-06-13 11:48:29 -0700

    search keywords on chrome browser

--- background.js
+++ background.js
@@ -1,5 +1,16 @@
 chrome.runtime.onInstalled.addListener((details) => {
-	console.log(details);
+	chrome.contextMenus.create({
+		title: "Test Context Menu",
+		id: "contextMenu1",
+		contexts: ["page", "selection"]
+	});
+	chrome.contextMenus.onClicked.addListener((event) => {
+		console.log(event);
+		chrome.search.query({
+			disposition: "NEW_TAB",
+			text: `imdb ${event.selectionText}`
+		});
+	});
 });
 
 console.log("background script running");
